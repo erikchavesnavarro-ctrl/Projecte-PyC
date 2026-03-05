3a5bf576c90fa69495856863fcbf729b347289ff
marioogc05 <mariogarca2005@gmail.com>
2026-03-05 16:19:29 +0100

    Commit AskData
    Commit prova

---
+++ ProjectePyC/src/main/java/com/mycompany/projectepyc/ProjectePyC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectepyc;
+
+/**
+ *
+ * @author Mario
+ */
+public class ProjectePyC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
