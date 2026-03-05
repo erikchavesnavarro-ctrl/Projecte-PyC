612324584945dcfb0901f6bea4e70749618b8c9a
echaves <erik.chaves.navarro@ieselcalamot.com>
2026-03-05 16:36:37 +0100

    Prova
    commit prova

---
+++ ProjectePyC/src/main/java/Classes/Participant.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author Mario
+ */
+public class Participant {
+    
+    private String ID;
+    private String nickname;
+
+    public Participant(String ID, String nickname) {
+        this.ID = ID;
+        this.nickname = nickname;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+}
