fbfb762370c84b705a8be2224449d1a77f277481
dgarrido219 <diego.garridog2@gmail.com>
2026-04-10 15:36:35 +0200

    Actualizar prova

---
+++ ProjectePyC/src/main/java/com/mycompany/projectepyc/model/Joc.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectepyc.model;
+
+/**
+ *
+ * @author willg
+ */
+public abstract class Joc {
+     protected int puntsTorneig;
+
+}
